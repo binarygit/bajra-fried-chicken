c2149cc4071b0333ef24364ebd2151b416dd666d
binarygit <inscription4@outlook.com>
2022-06-14 18:29:49 +0545

    write user#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(form_params)
+
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def form_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
