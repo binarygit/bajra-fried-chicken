8cfafe02a95225527ed74c14cf2065959fbc502a
binarygit <inscription4@outlook.com>
2022-06-14 18:52:23 +0545

    stop rendering field with errors div

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module Bfc
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Stop rendering field with errors div
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
+      html_tag
+    }
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
