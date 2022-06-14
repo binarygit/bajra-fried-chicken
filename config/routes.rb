427a1fb4b4408db29cdcb545d7aba2b77343b19a
binarygit <inscription4@outlook.com>
2022-06-14 18:25:58 +0545

    create and style users#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :sessions
+  resources :users
 
   root 'sessions#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
