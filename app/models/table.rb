1e02a1cb2fd51793ff29b3ae0d09c9f46a8d3965
binarygit <inscription4@outlook.com>
2022-06-15 09:56:53 +0545

    add validations for table

--- app/models/table.rb
+++ app/models/table.rb
@@ -1,4 +1,7 @@
 class Table < ApplicationRecord
   has_one :reservation
   has_one :user, through: :reservation
+
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
 end
