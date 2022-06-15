b9bd086ecaeed8c67ed3a3abd928353d3e4b08b6
binarygit <inscription4@outlook.com>
2022-06-15 09:56:06 +0545

    create association between user, table and reservation

--- app/models/table.rb
+++ app/models/table.rb
@@ -1,2 +1,4 @@
 class Table < ApplicationRecord
+  has_one :reservation
+  has_one :user, through: :reservation
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :reservations
+  has_many :tables, through: :reservations
+
   validates :email, :username, uniqueness: true, presence: true
   validates :password, confirmation: true, presence: true
   validates :password_confirmation, presence: true
