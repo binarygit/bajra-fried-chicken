5bc2f5656455cd686fe91af5217af73ca36cb42b
binarygit <inscription4@outlook.com>
2022-06-15 10:04:36 +0545

    create tables#index and set it as root

---
+++ app/controllers/tables_controller.rb
@@ -0,0 +1,21 @@
+class TablesController < ApplicationController
+  before_action :authenticate_user, only: :index
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy 
+  end
+
+  private
+
+  def authenticate_user
+    redirect_to login_path unless logged_in?
+  end
+end
