47fb74dfb9086cfbd101dadad6fac78ceb5b5bb1
binarygit <inscription4@outlook.com>
2022-06-15 09:35:26 +0545

    create Table model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_14_075442) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_15_034856) do
+  create_table "tables", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
