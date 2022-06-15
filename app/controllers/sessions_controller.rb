e0679cd2db19326b12a4ce5f70cbb4f25e712a46
binarygit <inscription4@outlook.com>
2022-06-15 08:11:47 +0545

    render error messages during login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: 'Logged In'
+    else
+      flash.now[:alert] = 'Email or password is invalid'
+      render :new, status: :unprocessable_entity
+    end
   end
 end
