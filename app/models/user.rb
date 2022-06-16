8620b648cc38bdc7d11928f5f09a9a0dec5fd0c4
binarygit <inscription4@outlook.com>
2022-06-16 13:14:39 +0545

    create orders table and migrate db

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   has_many :reservations
   has_many :tables, through: :reservations
 
+  has_many :orders
+  has_many :foods, through: :orders
+
   validates :email, :username, uniqueness: true, presence: true
   validates :password, confirmation: true, presence: true
   validates :password_confirmation, presence: true
