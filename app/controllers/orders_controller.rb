584accd39f7c4c1bb2354a21764e09961dac4500
binarygit <inscription4@outlook.com>
2022-06-17 18:09:22 +0545

    create orders and their bills

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,17 @@ class OrdersController < ApplicationController
 
   def create
     orders = JSON.parse(cookies[:orders])
-    return redirect_to foods_path if orders.empty?
+    unless orders.empty?
+      bill = Bill.new(user_id: current_user.id)
+      bill_total = 0
+      orders.each do |order|
+        bill_total += order['total']
+        bill.orders.build(order)
+      end
+      bill.update(total: bill_total)
+      flash.notice = 'Your order has been successfully placed.'
+    end
+    redirect_to foods_path
   end
 
   def destroy 
