845c56955612aa3d70c41fb88223de4cb8b2a17e
binarygit <inscription4@outlook.com>
2022-06-18 14:41:19 +0545

    create seed for foods

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Food.destroy_all
+
+Food.create!([
+  { name: 'momo', description: 'momo is momo!', price: 150 },
+  { name: 'meat lover pizza', description: 'pizzaaaaaaaa', price: 450 },
+  { name: 'Chicken Biryani', description: 'The best biryani in town is at Gurkha Grill.', price: 450 },
+])
