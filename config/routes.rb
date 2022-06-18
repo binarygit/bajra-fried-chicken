8e64fb252af013843fb5e6a3f44904c1ca76dc87
binarygit <inscription4@outlook.com>
2022-06-18 12:44:33 +0545

    create namespaced route for admin

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,12 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  namespace :admin do
+    root 'bills#index'
+    resources :bills
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
