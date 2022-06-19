f9b9c1b61456fc1f351e4030555a44caba08d854
binarygit <inscription4@outlook.com>
2022-06-19 16:00:21 +0545

    destroy orders when bill is destroyed

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,6 +1,6 @@
 class Bill < ApplicationRecord
   belongs_to :user
-  has_many :orders, inverse_of: :bill
+  has_many :orders, inverse_of: :bill, dependent: :destroy
   has_many :foods, through: :orders
 
   accepts_nested_attributes_for :orders
