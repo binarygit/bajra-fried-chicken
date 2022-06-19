9e68bfa9817ffaefbb582632bf2629dbd2168f6c
binarygit <inscription4@outlook.com>
2022-06-19 19:16:07 +0545

    tables can be deleted

--- app/models/table.rb
+++ app/models/table.rb
@@ -1,5 +1,5 @@
 class Table < ApplicationRecord
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
   has_many :users, through: :reservations
 
   validates :name, presence: true, uniqueness: true
