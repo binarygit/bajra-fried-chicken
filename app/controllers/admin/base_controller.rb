cee634c8c14b4be18a31dacb23e8c9873f4e6632
binarygit <inscription4@outlook.com>
2022-06-20 22:09:14 +0545

    donot let normal users access admin urls

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,11 @@
 class Admin::BaseController < ApplicationController
   layout 'admin'
+  before_action :verify_admin
+
+  private
+
+  def verify_admin
+    flash.alert = 'Unauthorized Access.'
+    redirect_back_or_to root_path unless admin?
+  end
 end
